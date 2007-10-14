bd0ededd3387983226138b47b53902f2b32a7145
Eric Davis <edavis@littlestreamsoftware.com>
2007-10-14 04:41:32 +0000

    * Cleaning up the report and added the date range

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -14,12 +14,16 @@ class TimesheetController < ApplicationController
       sort_init 'spent_on', 'desc'
       sort_update
 
-      date = Date.civil(params[:filter][:"date(1i)"].to_i,params[:filter][:"date(2i)"].to_i,params[:filter][:"date(3i)"].to_i)
+      from_date = Date.civil(params[:from][:"date(1i)"].to_i,params[:from][:"date(2i)"].to_i,params[:from][:"date(3i)"].to_i)
+      to_date = Date.civil(params[:to][:"date(1i)"].to_i,params[:to][:"date(2i)"].to_i,params[:to][:"date(3i)"].to_i)
 
       @projects = Project.find(:all);
       @entries = { }
       @projects.each do |project|
-        logs = project.time_entries.find(:all, :conditions => ['spent_on > (?)',date], :include => [:activity, :user, {:issue => [:tracker, :assigned_to, :priority]}], :order => sort_clause)
+        logs = project.time_entries.find(:all,
+                                         :conditions => ['spent_on >= (?) AND spent_on <= (?)',from_date,to_date],
+                                         :include => [:activity, :user, {:issue => [:tracker, :assigned_to, :priority]}],
+                                         :order => "spent_on ASC")
         @entries[project.name] = logs unless logs.empty?
       end
 
@@ -29,6 +33,7 @@ class TimesheetController < ApplicationController
       render :action => 'details', :layout => false if request.xhr?
     when :get
       # nothing
+      @entries = []
     end
   end
 
