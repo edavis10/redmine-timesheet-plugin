d65f59fbaa009b03c86140930adb0647d21128b8
Eric Davis <edavis@littlestreamsoftware.com>
2007-11-30 06:49:40 +0000

    * Removed @owner_id

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -39,7 +39,7 @@ class TimesheetController < ApplicationController
         end
      end
 
-      @owner_id = logged_in_user ? logged_in_user.id : 0
+#      @owner_id = logged_in_user ? logged_in_user.id : 0
 
       send_csv and return if 'csv' == params[:export]
       render :action => 'details', :layout => false if request.xhr?
