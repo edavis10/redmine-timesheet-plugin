96ba0a14b4037e330c560d97e5ddc978fda87ce1
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-15 14:09:04 -0700

    Implemented spec for having the project name as the time_entry array

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -74,11 +74,39 @@ describe Timesheet,'.fetch_time_entries' do
     
     timesheet.fetch_time_entries
     timesheet.time_entries.should_not be_empty
+    timesheet.time_entries.should have(2).things
+  end
+  
+  it 'should use the project name for each time_entry array' do 
+    timesheet = Timesheet.new
+    timesheet.date_from = Date.today.to_s
+    timesheet.date_to = Date.today.to_s
+
+    # First project
+    project1 = mock_model(Project, :parent => nil)
+    project1_te1 = mock_model(TimeEntry, :id => '100', :project_id => project1.id, :issue_id => '1', :hours => '5', :activity_id => '1', :spent_on => Date.today)
+    project1_te2 = mock_model(TimeEntry, :id => '101', :project_id => project1.id, :issue_id => '1', :hours => '10', :activity_id => '1', :spent_on => Date.today)
+    project1_time_entries_mock = mock('project1_time_entries_mock')
+    project1_time_entries_mock.stub!(:find).and_return([project1_te1, project1_te2])
+    project1.stub!(:time_entries).and_return(project1_time_entries_mock)
+    project1.should_receive(:name).and_return('Project one')
+    
+    # Second project
+    project2 = mock_model(Project, :parent => nil)
+    project2_te1 = mock_model(TimeEntry, :id => '100', :project_id => project2.id, :issue_id => '2', :hours => '5', :activity_id => '1', :spent_on => Date.today)
+    project2_te2 = mock_model(TimeEntry, :id => '101', :project_id => project2.id, :issue_id => '2', :hours => '10', :activity_id => '1', :spent_on => Date.today)
+    project2_time_entries_mock = mock('project2_time_entries_mock')
+    project2_time_entries_mock.stub!(:find).and_return([project2_te1, project2_te2])
+    project2.stub!(:time_entries).and_return(project2_time_entries_mock)
+    project2.should_receive(:name).and_return('Project two')
+    
+    
+    timesheet.projects = [project1, project2]
+    
+    timesheet.fetch_time_entries
     timesheet.time_entries.should include("Project one")
     timesheet.time_entries.should include("Project two")
   end
-  
-  it 'should use the project name for each time_entry array'
   it 'should add the parent project name for each time_entry array for sub-projects'
   it 'should fetch all the time entries on a project in the date range'
   it 'should fetch all the time entries on a project matching the activities'
