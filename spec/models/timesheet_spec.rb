1a2187cd5d712aa415ea08f3eade64c510d9f51d
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-15 14:14:32 -0700

    Refactored specs to a factory pattern. #812

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -1,5 +1,20 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+module TimesheetSpecHelper
+  def project_factory(id)
+    # First project
+    project = mock_model(Project, :parent => nil, :id => id, :to_param => id.to_s)
+    project_te1 = mock_model(TimeEntry, :id => '100' + id.to_s, :project_id => project.id, :issue_id => '1', :hours => '5', :activity_id => '1', :spent_on => Date.today)
+    project_te2 = mock_model(TimeEntry, :id => '101' + id.to_s, :project_id => project.id, :issue_id => '1', :hours => '10', :activity_id => '1', :spent_on => Date.today)
+    project_time_entries_mock = mock('project_time_entries_mock')
+    project_time_entries_mock.stub!(:find).and_return([project_te1, project_te2])
+    project.stub!(:time_entries).and_return(project_time_entries_mock)
+    project.stub!(:name).and_return('Project ' + id.to_s)
+    
+    return project
+  end
+end
+
 describe Timesheet do
   it 'should not be an ActiveRecord class' do
     Timesheet.should_not be_a_kind_of(ActiveRecord::Base)
@@ -37,6 +52,8 @@ describe Timesheet do
 end
 
 describe Timesheet,'.fetch_time_entries' do
+  include TimesheetSpecHelper
+  
   it 'should clear .time_entries' do
     timesheet = Timesheet.new
     timesheet.time_entries = { :filled => 'data' }
@@ -51,25 +68,9 @@ describe Timesheet,'.fetch_time_entries' do
     timesheet.date_from = Date.today.to_s
     timesheet.date_to = Date.today.to_s
 
-    # First project
-    project1 = mock_model(Project, :parent => nil)
-    project1_te1 = mock_model(TimeEntry, :id => '100', :project_id => project1.id, :issue_id => '1', :hours => '5', :activity_id => '1', :spent_on => Date.today)
-    project1_te2 = mock_model(TimeEntry, :id => '101', :project_id => project1.id, :issue_id => '1', :hours => '10', :activity_id => '1', :spent_on => Date.today)
-    project1_time_entries_mock = mock('project1_time_entries_mock')
-    project1_time_entries_mock.stub!(:find).and_return([project1_te1, project1_te2])
-    project1.stub!(:time_entries).and_return(project1_time_entries_mock)
-    project1.should_receive(:name).and_return('Project one')
-    
-    # Second project
-    project2 = mock_model(Project, :parent => nil)
-    project2_te1 = mock_model(TimeEntry, :id => '100', :project_id => project2.id, :issue_id => '2', :hours => '5', :activity_id => '1', :spent_on => Date.today)
-    project2_te2 = mock_model(TimeEntry, :id => '101', :project_id => project2.id, :issue_id => '2', :hours => '10', :activity_id => '1', :spent_on => Date.today)
-    project2_time_entries_mock = mock('project2_time_entries_mock')
-    project2_time_entries_mock.stub!(:find).and_return([project2_te1, project2_te2])
-    project2.stub!(:time_entries).and_return(project2_time_entries_mock)
-    project2.should_receive(:name).and_return('Project two')
-    
-    
+    project1 = project_factory(1)
+    project2 = project_factory(2)
+
     timesheet.projects = [project1, project2]
     
     timesheet.fetch_time_entries
@@ -82,31 +83,18 @@ describe Timesheet,'.fetch_time_entries' do
     timesheet.date_from = Date.today.to_s
     timesheet.date_to = Date.today.to_s
 
-    # First project
-    project1 = mock_model(Project, :parent => nil)
-    project1_te1 = mock_model(TimeEntry, :id => '100', :project_id => project1.id, :issue_id => '1', :hours => '5', :activity_id => '1', :spent_on => Date.today)
-    project1_te2 = mock_model(TimeEntry, :id => '101', :project_id => project1.id, :issue_id => '1', :hours => '10', :activity_id => '1', :spent_on => Date.today)
-    project1_time_entries_mock = mock('project1_time_entries_mock')
-    project1_time_entries_mock.stub!(:find).and_return([project1_te1, project1_te2])
-    project1.stub!(:time_entries).and_return(project1_time_entries_mock)
-    project1.should_receive(:name).and_return('Project one')
-    
-    # Second project
-    project2 = mock_model(Project, :parent => nil)
-    project2_te1 = mock_model(TimeEntry, :id => '100', :project_id => project2.id, :issue_id => '2', :hours => '5', :activity_id => '1', :spent_on => Date.today)
-    project2_te2 = mock_model(TimeEntry, :id => '101', :project_id => project2.id, :issue_id => '2', :hours => '10', :activity_id => '1', :spent_on => Date.today)
-    project2_time_entries_mock = mock('project2_time_entries_mock')
-    project2_time_entries_mock.stub!(:find).and_return([project2_te1, project2_te2])
-    project2.stub!(:time_entries).and_return(project2_time_entries_mock)
-    project2.should_receive(:name).and_return('Project two')
-    
-    
+    project1 = project_factory(1)
+    project1.should_receive(:name).and_return('Project 1')
+    project2 = project_factory(2)
+    project2.should_receive(:name).and_return('Project 2')
+
     timesheet.projects = [project1, project2]
     
     timesheet.fetch_time_entries
-    timesheet.time_entries.should include("Project one")
-    timesheet.time_entries.should include("Project two")
+    timesheet.time_entries.should include("Project 1")
+    timesheet.time_entries.should include("Project 2")
   end
+
   it 'should add the parent project name for each time_entry array for sub-projects'
   it 'should fetch all the time entries on a project in the date range'
   it 'should fetch all the time entries on a project matching the activities'
