f018ed97c55c22f0e148f7e6bd57c44608533143
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-17 14:50:54 -0700

    Added helper to generate a GET link to the timesheet and added it to the contextual menu.

--- app/helpers/timesheet_helper.rb
+++ app/helpers/timesheet_helper.rb
@@ -2,4 +2,17 @@ module TimesheetHelper
   def showing_users(users)
     l(:timesheet_showing_users) + users.collect(&:name).join(', ')
   end
+
+  def permalink_to_timesheet(timesheet)
+    link_to(l(:timesheet_permalink),
+            :controller => 'timesheet',
+            :action => 'report',
+            :timesheet => { 
+              :projects => timesheet.projects.collect(&:id),
+              :date_from => timesheet.date_from,
+              :date_to => timesheet.date_to,
+              :activities => timesheet.activities,
+              :users => timesheet.users
+            })
+  end
 end
