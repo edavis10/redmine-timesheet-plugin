3a196e8aba5a0687430180237dc14136a70a9c7e
Eric Davis <edavis@littlestreamsoftware.com>
2009-01-12 13:59:41 -0800

    Added plugin urls and requiring Redmine 0.8.0+ in order to use this version (uses hooks).

--- init.rb
+++ init.rb
@@ -1,12 +1,14 @@
 require 'redmine'
 
-RAILS_DEFAULT_LOGGER.info 'Starting Timesheet plugin for Redmine'
-
 Redmine::Plugin.register :timesheet_plugin do
   name 'Timesheet Plugin'
   author 'Eric Davis of Little Stream Software'
   description 'This is a Timesheet plugin for Redmine to show timelogs for all projects'
+  url 'https://projects.littlestreamsoftware.com/projects/show/redmine-timesheet'
+  author_url 'http://www.littlestreamsoftware.com'
+
   version '0.3.0'
+  requires_redmine :version_or_higher => '0.8.0'
   
   settings :default => {'list_size' => '5', 'precision' => '2'}, :partial => 'settings/timesheet_settings'
 
