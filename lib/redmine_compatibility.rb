4feb66d849d891fdebeecb2373abfb94f309af81
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-04 09:57:31 -0800

    Fixed the compatibility module

--- lib/redmine_compatibility.rb
+++ lib/redmine_compatibility.rb
@@ -4,7 +4,7 @@ module RedmineCompatibility
     # Wrapper around Redmine's API since Enumerations changed in r2472
     # This can be removed once 0.9.0 is stable
     def self.activities
-      if Enumeration.respond_to?(:activities)
+      if ::Enumeration.respond_to?(:activities)
         return ::Enumeration.activities
       else
         return ::Enumeration::get_values('ACTI')
