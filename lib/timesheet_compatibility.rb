e1be50ae791e3d6d107dc54e6abc020eab0f0238
Eric Davis <edavis@littlestreamsoftware.com>
2009-11-11 14:33:08 -0800

    Use the newer TimeEntryActivities

--- lib/timesheet_compatibility.rb
+++ lib/timesheet_compatibility.rb
@@ -4,7 +4,9 @@ module TimesheetCompatibility
     # Wrapper around Redmine's API since Enumerations changed in r2472
     # This can be removed once 0.9.0 is stable
     def self.activities
-      if ::Enumeration.respond_to?(:activities)
+      if Object.const_defined?('TimeEntryActivity')
+        return ::TimeEntryActivity.all
+      elsif ::Enumeration.respond_to?(:activities)
         return ::Enumeration.activities
       else
         return ::Enumeration::get_values('ACTI')
