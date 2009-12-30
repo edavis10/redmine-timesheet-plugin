cb501d9c5bae28b2f2d00ceabd40274b126e9565
Eric Davis <edavis@littlestreamsoftware.com>
2009-12-30 14:53:06 -0800

    [#3259] Removed the ruby-debug requirement

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
 require 'spec'
 require 'spec/rails'
-require 'ruby-debug'
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
