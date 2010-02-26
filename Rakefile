bd80577c17b91ffc1a1328cae032e199d522cf4c
Eric Davis <edavis@littlestreamsoftware.com>
2010-02-25 21:39:06 -0800

    [#3506] Added Test::Unit framework

--- Rakefile
+++ Rakefile
@@ -6,7 +6,7 @@ Dir[File.expand_path(File.dirname(__FILE__)) + "/lib/tasks/**/*.rake"].sort.each
 RedminePluginSupport::Base.setup do |plugin|
   plugin.project_name = 'timesheet_plugin'
   plugin.default_task = [:spec]
-  plugin.tasks = [:doc, :release, :clean, :spec, :stats]
+  plugin.tasks = [:doc, :release, :clean, :spec, :stats, :test, :db]
   # TODO: gem not getting this automaticly
   plugin.redmine_root = File.expand_path(File.dirname(__FILE__) + '/../../../')
 end
---
+++ test/unit/sanity_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SanityTest < ActiveSupport::TestCase
+  def test_is_sane
+    assert true
+  end
+
+  should "be true" do
+    assert true
+  end
+
+  should "mixin ObjectDaddy" do
+    assert User.included_modules.include?(ObjectDaddy)
+  end
+
+  should "connect to database" do
+    User.generate_with_protected!(:firstname => 'Testing connection')
+    assert_equal 1, User.count(:all, :conditions => {:firstname => 'Testing connection'})
+  end
+end
