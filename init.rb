76e9dbbef15abe0b4dae32c971b6baaccd2bdd3f
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-18 20:45:02 -0700

    [#3790] Bumping Redmine version number.

--- init.rb
+++ init.rb
@@ -27,8 +27,8 @@ unless Redmine::Plugin.registered_plugins.keys.include?(:timesheet_plugin)
     url 'https://projects.littlestreamsoftware.com/projects/redmine-timesheet'
     author_url 'http://www.littlestreamsoftware.com'
 
-    version '0.5.0'
-    requires_redmine :version_or_higher => '0.8.0'
+    version '0.6.0'
+    requires_redmine :version_or_higher => '0.8.7'
     
     settings :default => {'list_size' => '5', 'precision' => '2'}, :partial => 'settings/timesheet_settings'
 
