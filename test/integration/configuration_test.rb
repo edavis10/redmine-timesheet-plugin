4fa7c29267fcd8b47c339b977ea870f696ca2977
Eric Davis <edavis@littlestreamsoftware.com>
2011-04-21 08:58:52 -0700

    [#5778] Added a test for configuring the plugin

---
+++ test/integration/configuration_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class ConfigurationTest < ActionController::IntegrationTest
+  def setup
+    @user = User.generate!(:password => 'test', :password_confirmation => 'test', :admin => true)
+  end
+
+  should "add a plugin configuration panel" do
+    login_as(@user.login, 'test')
+    visit_home
+    click_link 'Administration'
+    assert_response :success
+
+    click_link 'Plugins'
+    assert_response :success
+
+    click_link 'Configure'
+    assert_response :success
+  end
+
+  should "be able to configure the list size" do
+    login_as(@user.login, 'test')
+    visit_configuration_panel
+
+    fill_in "List size", :with => '10'
+    click_button 'Apply'
+
+    assert_equal '10', plugin_configuration['list_size']
+  end
+
+  should "be able to configure the number precision" do
+    login_as(@user.login, 'test')
+    visit_configuration_panel
+
+    fill_in "Number precision", :with => '10'
+    click_button 'Apply'
+
+    assert_equal '10', plugin_configuration['precision']
+  end
+
+  should "be able to configure the project status" do
+    login_as(@user.login, 'test')
+    visit_configuration_panel
+
+    select "All (active and archived)", :from => 'settings_project_status'
+    click_button 'Apply'
+
+    assert_equal 'all', plugin_configuration['project_status']
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,3 +28,87 @@ class Test::Unit::TestCase
     end
   end
 end
+
+def User.add_to_project(user, project, role)
+  Member.generate!(:principal => user, :project => project, :roles => [role])
+end
+
+module ChiliProjectIntegrationTestHelper
+  def login_as(user="existing", password="existing")
+    visit "/logout" # Make sure the session is cleared
+
+    visit "/login"
+    fill_in 'Login', :with => user
+    fill_in 'Password', :with => password
+    click_button 'Login'
+    assert_response :success
+    assert User.current.logged?
+  end
+
+  def visit_home
+    visit '/'
+    assert_response :success
+  end
+
+  def visit_project(project)
+    visit '/'
+    assert_response :success
+
+    click_link 'Projects'
+    assert_response :success
+
+    click_link project.name
+    assert_response :success
+  end
+
+  def visit_issue_page(issue)
+    visit '/issues/' + issue.id.to_s
+  end
+
+  def visit_issue_bulk_edit_page(issues)
+    visit url_for(:controller => 'issues', :action => 'bulk_edit', :ids => issues.collect(&:id))
+  end
+end
+
+module TimesheetIntegrationTestHelper
+  def visit_configuration_panel
+    visit_home
+    click_link 'Administration'
+    assert_response :success
+
+    click_link 'Plugins'
+    assert_response :success
+
+    click_link 'Configure'
+    assert_response :success
+  end
+  
+end
+
+class ActionController::IntegrationTest
+  include ChiliProjectIntegrationTestHelper
+  include TimesheetIntegrationTestHelper
+end
+
+class ActiveSupport::TestCase
+  def assert_forbidden
+    assert_response :forbidden
+    assert_template 'common/403'
+  end
+
+  def configure_plugin(configuration_change={})
+    Setting.plugin_timesheet_plugin = {
+      'list_size' => '5',
+      'precision' => '2',
+      'project_status' => 'active'
+    }.merge(configuration_change)
+  end
+
+  def reconfigure_plugin(configuration_change)
+    Settings['plugin_timesheet_plugin'] = Setting['plugin_timesheet_plugin'].merge(configuration_change)
+  end
+
+  def plugin_configuration
+    Setting.plugin_timesheet_plugin
+  end
+end
